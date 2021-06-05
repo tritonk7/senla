8154c1fc06aed16200a3319ab2e468b4f372af1e
tritonk7 <tritonk9@gmail.com>
2021-06-05 23:52:41 +0300

    Added Hyrerlink for Name

--- Lecture_3.4/Aura_Component/TripRecordPageController.js
+++ Lecture_3.4/Aura_Component/TripRecordPageController.js
@@ -1,14 +1,25 @@
 ({
     doInit: function (component, event, helper) {
         component.set('v.columns', [
-            {label: 'Tourist Name', fieldName: 'Name', type: 'url', typeAttributes: {label: { fieldName: 'Name' }, target: '_parent'}},
+            {label: 'Tourist Name', fieldName: 'linkName', type: 'url', 
+             typeAttributes: { label: { fieldName: 'Name' }, target: '_self', tooltip: {fieldName: 'Name' }}},
             {label: 'Email', fieldName: 'Tourist_Email__c', type: 'email'},
             {label: 'Gender', fieldName: 'Gender__c', type: 'picklist'}
         ]);
-        helper.fetchTourists(component, event);
+        helper.fetchTourists (component, event);   
     },
     
     createFlight: function(component, event, helper) {
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            title : 'Success Toast Message',
+            message: 'Selected Tourists will be assigned to the Trip.',
+            duration:' 4000',
+            key: 'info_alt',
+            type: 'success',
+            mode: 'pester'
+        });
+        toastEvent.fire();
         helper.fetchFlights(component, event);
     },
     
@@ -19,7 +30,6 @@
             setRows.push(selectedRows[i]);
         }
         component.set("v.selectedTourists", setRows);
-        
     },
     
     handleShowSpinner: function(component, event, helper) {
--- Lecture_3.4/Aura_Component/TripRecordPageHelper.js
+++ Lecture_3.4/Aura_Component/TripRecordPageHelper.js
@@ -1,50 +1,39 @@
 ({
     fetchTourists : function(component, event) {
         var action = component.get("c.fetchTourist");
-       
         action.setParams({
             tripId: component.get("v.recordId")
         });
-       
-        action.setCallback(this, function(response) {
+        action.setCallback(this, function(response){
             var state = response.getState();
-            if (state === 'SUCCESS') {
-                var responseValue = response.getReturnValue();
+            if (state === "SUCCESS") {
+                var records =response.getReturnValue();
+                records.forEach(function(record){
+                    record.linkName = '/'+ record.Id;
+                });
+                component.set("v.data", records);
             }
-            
-             component.set("v.data", responseValue);
         });
-        
         $A.enqueueAction(action);
     },
     
     fetchFlights : function(component, event) {
-       
         var records = component.get("v.selectedTourists");
         var ids = new Array();
         
         for ( var i = 0; i < records.length; i++ ) {
-            alert(records[i].Id);
             ids.push(records[i].Id);
         } 
-        
-        
         var action = component.get("c.saveFlight");
-      
         action.setParams({tripId: component.get("v.recordId"),
                           touristIds: ids
                          });
-            
-            action.setCallback(this, function(response) {
-                var state = response.getState();
-                if (state === 'SUCCESS') {
-                    var name = response.getReturnValue();
-                    alert('Flights did created');
-                }
-            });
-            
-            $A.enqueueAction(action);
-        
-    }
-  
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === 'SUCCESS') {
+                var name = response.getReturnValue();
+            }
+        });
+        $A.enqueueAction(action);
+    }  
 })
