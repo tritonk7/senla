1e83370a484a377bd99c6a464c114e61a59731a8
tritonk7 <tritonk9@gmail.com>
2021-06-14 00:44:30 +0300

    Added look - up for tourist

--- Lecture_3.4/Aura_Component/TouristAssigmentCmpController.js
+++ Lecture_3.4/Aura_Component/TouristAssigmentCmpController.js
@@ -1,17 +1,10 @@
-({
+{
     doInit : function(component, event, helper) {
-        component.set("v.tourist", [
-            {label: "Tourist Name", fieldName: "linkName", type: "url", 
-             typeAttributes: { label: { fieldName: "Name" }, target: "_self", tooltip: {fieldName: "Name" }}},
-            {label: 'Age', fieldName: 'Tourist_Age__c', type: 'Integer'} 
-        ]);
         component.set("v.columns", [
             {label: "Trip Name", fieldName: "linkName", type: "url", 
              typeAttributes: { label: { fieldName: 'Name' }, target: "_self", tooltip: {fieldName: "Name" }}},
             {label: 'Start Date', fieldName: 'Start_Data__c', type: 'Date'}
         ]);
-        helper.fetchTourists(component, event);
-        helper.fetchTrip(component, event);
         helper.fetchFlights(component, event);
         helper.fetchWeather(component, event);
         let today = $A.localizationService.formatDate(new Date(), "YYYY-MM-DD");
@@ -19,65 +12,69 @@
     },
     
     handleSelectTourist: function (component, event) {
-        let selectedRows = event.getParam("selectedRows");
-        component.set("v.touristAge", selectedRows[0].Tourist_Age__c);
-        component.set("v.touristId", selectedRows[0].Id);
-        if (selectedRows.length > 0) {
-            component.set("v.isVisible", true);
-        }else {
+        let ids = component.get("v.selectedLookUpRecord");
+        let touristId = component.set("v.touristId", ids.Id);
+        let touristAge = component.set("v.touristAge", ids.Tourist_Age__c);
+    },
+    
+    selectedTrip: function (component, event) {
+        let action = component.get("c.fetchTrip");
+        let id = component.get("v.touristId");
+        if (typeof(id) == "undefined") {
+            component.set("v.isVisibleButton", false);
             component.set("v.isVisible", false);
-        } 
+            component.set("v.isVisibleMap", false);
+        } else {
+            component.set("v.isVisible", true);
+        }
+        action.setParams({
+            touristsId : id
+        });
+        action.setCallback(this, function(response) {
+            let state = response.getState();
+            if (state === "SUCCESS") {
+                let records = response.getReturnValue();
+                records.forEach(function(record) {
+                    record.linkName = '/' + record.Id;
+                });
+                component.set("v.data", records);
+            }
+        });
+        $A.enqueueAction(action); 
     },
     
     handleSelect : function(component, event, helper) {
         helper.fetchTripSelectedRows(component, event);
-        let flight  = component.get("v.flightData");
         let tripId = component.get("v.tripId");
-        let busySeats = [];
+        let tripsId =  [];
+        let flights = component.get("v.flightData");
+        for (let i = 0; i < flights.length; i++) {
+            if (tripId == flights[i].Trip__c) {
+                tripsId.push(flights[i].Trip__c); 
+            }
+        }
+        component.set("v.freeSeats", component.get("v.setSeats") - tripsId.length);
         let weather = component.get("v.weather");
         let city = component.get("v.setSpacePoint")
-        let touristId = component.get("v.touristId");
         
         for (let i = 0; i < weather.length; i++) {
             if (city == weather[i].Space_Point__r.City__c) {
                 component.set("v.setTemperature", weather[i].Average_Temperature__c);
             }
-        }
-        
-        if (flight.length == 0) {
-            component.set("v.busySeats",0);
-        }else {
-            
-            for (let i = 0; i < flight.length; i++) {
-                if (tripId == flight[i].Trip__c && touristId == flight[i].New_Tourist__c) {
-                    busySeats.push(flight[i].Trip__c);
-                    component.set("v.busySeats",busySeats.length);
-                    component.set("v.isCheckTourist",true);
-                } else {
-                    component.set("v.busySeats",0);
-                    component.set("v.isCheckTourist",false);
-                }  
-            }
-        }
-        let freeSeats  = component.get("v.setSeats") - component.get("v.busySeats");
-        component.set("v.setFreeSeats",freeSeats);
-        
+        } 
     },
     
-    onChangeTourist : function(component, event, helper) {
+    onChange : function(component, event, helper) {
+        component.set("v.isVisibleButton", false);
         let touristAge = component.get("v.touristAge");
-        let tripAge = component.get('v.minimumAge');
+        let tripAge = component.get("v.minimumAge");
         let city = component.get("v.setSpacePoint");
-        let check = typeof city;
-        let freeSeats = component.get("v.setFreeSeats");
-        let checkTourist = component.get("v.isCheckTourist");
-    
-        if (touristAge >= tripAge && freeSeats > 0 && !checkTourist) {
+        if (touristAge >= tripAge && component.get("v.freeSeats") > 0) {
             component.set("v.isVisibleButton", true);
         } else {
             component.set("v.isVisibleButton", false);
         }
-        if (check != "undefined" ) {
+        if (typeof(city) != "undefined") {
             component.set("v.isVisibleMap", true);
             component.set("v.mapMarkers", [
                 {
--- Lecture_3.4/Aura_Component/TripRecordPageHelper.js
+++ Lecture_3.4/Aura_Component/TripRecordPageHelper.js
@@ -1,149 +1,68 @@
-({
-    fetchTourists : function(component, event) {
-        let action = component.get("c.fetchTourist");
+({    
+    fetchFlights : function(component, event) {
+        let action = component.get("c.fetchFlights");
+        action.setCallback(this, function(response) {
+            let state = response.getState();
+            if (state === "SUCCESS") {
+                let records = response.getReturnValue();
+                if (records.length > 0) {
+                    component.set("v.flightData", records);
+                } else {
+                    component.set("v.flightData", []);
+                }
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    
+    fetchWeather : function(component, event) {
+        let action = component.get("c.fetchWeather");
+        action.setCallback(this, function(response) {
+            let state = response.getState();
+            if (state === "SUCCESS") {
+                let records = response.getReturnValue();
+                if (records.length > 0) {
+                    component.set("v.weather", records);
+                } 
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    
+    fetchTripSelectedRows : function (component, event) {
+        let selectedRows = event.getParam("selectedRows"); 
+        component.set('v.tripId',selectedRows[0].Id);
+        component.set('v.setStartDate',selectedRows[0].Start_Data__c);
+        component.set('v.setEndDate',selectedRows[0].End_Date__c);
+        component.set('v.tripName',selectedRows[0].Name);
+        component.set('v.minimumAge',selectedRows[0].Mininmum_Age__c);
+        component.set('v.setSpacePoint',selectedRows[0].Departure_Space_Point__r.City__c);
+        component.set('v.setSeats',selectedRows[0].Seats__c);
+    },
+    
+    handleCreateFlight : function(component, event) {
+        let touristId =component.get("v.touristId");
+        let tripId =component.get("v.tripId");
+        let action = component.get("c.createFlight");
         action.setParams({
-            tripId : component.get("v.recordId")
+            tripId: tripId,
+            touristId: touristId
         });
         action.setCallback(this, function(response) {
             let state = response.getState();
             if (state === "SUCCESS") {
                 let records = response.getReturnValue();
-                records.forEach(function(record) {
-                    record.linkName = '/' + record.Id;
+                let toastEvent = $A.get("e.force:showToast");
+                toastEvent.setParams({
+                    message: $A.get("$Label.c.Added_Tourists"),
+                    duration:' 4000',
+                    type: 'success',
+                    mode: 'pester'
                 });
-                component.set("v.data", records);
+                toastEvent.fire();
+                $A.get('e.force:refreshView').fire();
             }
         });
         $A.enqueueAction(action);
-    },
-    
-    fetchFlights : function(component, event) {
-        let updateTable = component.get("c.dataTableUpdate");
-        let records = component.get("v.selectedTourists");
-        let ids = [];
-        
-        for (let i = 0; i < records.length; i++) {
-            ids.push(records[i].Id);
-        } 
-        let action = component.get("c.saveFlight");
-        action.setParams({tripId : component.get("v.recordId"),
-                          touristIds : ids
-                         });
-        action.setCallback(this, function(response) {
-            let state = response.getState();
-            if (state === "SUCCESS") {
-                let result = response.getReturnValue();
-                if (result.length > 0) {
-                    let toastEvent = $A.get("e.force:showToast");
-                    toastEvent.setParams({
-                        message: $A.get("$Label.c.Added_Tourists"),
-                        duration:' 4000',
-                        type: 'success',
-                        mode: 'pester'
-                    });
-                    toastEvent.fire();
-                } else {
-                    let toastEvent = $A.get("e.force:showToast");
-                    toastEvent.setParams({
-                        message: $A.get("$Label.c.Error_Trip"),
-                        duration:' 4000',
-                        key: 'info_alt',
-                        type: 'error',
-                        mode: 'pester'
-                    });
-                    toastEvent.fire();
-                }
-            }
-        });
-        $A.enqueueAction(action);
-        $A.enqueueAction(updateTable);
-    },
-    
-    fetchValidation : function(component, event) {
-        let selectedRows = component.get("v.selectedTourists");
-        let seats = component.get("v.setSeats");
-        let setRow = [];
-        let startDate = component.get("v.setStartDate");
-        let today = component.get("v.setToday");
-        
-        for (let i = 0; i < selectedRows.length; i++) {
-            setRow.push(selectedRows[i]);
-        } 
-        
-        if (setRow.length > seats || setRow.length == 0) {
-            if (setRow.length > seats) {
-                let toastEvent = $A.get("e.force:showToast");
-                toastEvent.setParams({
-                    message: $A.get("$Label.c.NotEnoughtlSeats"),
-                    duration:' 4000',
-                    type: 'error',
-                    mode: 'pester'
-                });
-                toastEvent.fire(); 
-            }
-            if (setRow.length == 0) {
-                let toastEvent = $A.get("e.force:showToast");
-                toastEvent.setParams({
-                    message:  $A.get("$Label.c.TouristsIsNotSelected"),
-                    duration:' 4000',
-                    type: 'error',
-                    mode: 'pester'
-                });
-                toastEvent.fire(); 
-            } 
-        } else {
-            component.set("v.showConfirmDialog", true);  
-        } 
-    },
-    
-    fetchSeats : function(component, event) {
-        let freeSeats = component.get("c.getSeats");
-        freeSeats.setParams({
-            tripId : component.get("v.recordId")
-        });
-        freeSeats.setCallback(this, function(response) {
-            let state = response.getState();
-            if (state === "SUCCESS") {
-                let result = response.getReturnValue();
-                component.set("v.setSeats", result);
-            }
-        });
-        $A.enqueueAction(freeSeats);
-    },
-    
-    fetchStartDate : function(component, event) {
-        let freeSeats = component.get("c.getStartDate");
-        freeSeats.setParams({
-            tripId : component.get("v.recordId")
-        });
-        freeSeats.setCallback(this, function(response) {
-            let state = response.getState();
-            if (state === "SUCCESS") {
-                let result = response.getReturnValue();
-                component.set("v.setStartDate", result);
-            }
-        });
-        $A.enqueueAction(freeSeats); 
-    },
-    
-    fetchSelectedRows : function (component, event) {
-        let selectedRows = event.getParam("selectedRows"); 
-        let setRows = [];
-        
-        for (let i = 0; i < selectedRows.length; i++) {
-            setRows.push(selectedRows[i]);
-        }
-        component.set("v.selectedTourists", setRows);
-    },
-    
-    fetchVisibleButton : function (component, event) {
-        let seats = component.get("v.setSeats");
-        let today = component.get("v.setToday");
-        let startDate = component.get("v.setStartDate");
-        if (seats <= 0 || startDate <= today) {
-            component.set("v.isVisibleButton",false);
-        } else {
-            component.set("v.isVisibleButton",true);
-        }
     }
 })
