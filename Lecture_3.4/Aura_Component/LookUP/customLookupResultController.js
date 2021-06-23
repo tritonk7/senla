470b05045b9e23dbeab0e9c8759b502602884049
tritonk7 <tritonk9@gmail.com>
2021-06-23 05:45:00 +0300

    Remove Custom Look Up
    Remove Custom Look Up

--- Lecture_3.4/Aura_Component/LookUP/customLookupResultController.js
+++
@@ -1,8 +0,0 @@
-({
-    selectRecord : function(component, event, helper){      
-        let getSelectRecord = component.get("v.oRecord");
-        let compEvent = component.getEvent("oSelectedRecordEvent");
-        compEvent.setParams({"recordByEvent" : getSelectRecord });  
-        compEvent.fire();
-    },
-})
